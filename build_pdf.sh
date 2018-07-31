d02a38b326fbb0a8ee03c1d34a5d3b70001c4a3e
Fairiz Azizi <coderfi@gmail.com>
2018-07-31 01:11:09 -0700

    use docker

--- build_pdf.sh
+++ build_pdf.sh
@@ -2,20 +2,14 @@
 
 # builds the PDF
 
-# setup notes:
-# gem install --user asciidoctor
-# gem install --user asciidoctor-pdf --pre
-# gem install --user rouge
-# gem install --user pygments.rb
-# gem install --user coderay
-
 TAG=$(git symbolic-ref --short HEAD)
 FILENAME="Mastering Ethereum r${TAG}.pdf"
 
 git checkout book.asciidoc
 perl -pi -e "s/^= Mastering Ethereum$/= Mastering Ethereum r${TAG}/g" book.asciidoc
 perl -pi -e "s/:pdf-page-size: 8in x 9in/:pdf-page-size: 8in x 14in/g" book.asciidoc
-asciidoctor -r asciidoctor-pdf -b pdf -o book.pdf book.asciidoc
+
+docker run --rm -it -v `pwd`:/documents/ asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-pdf -b pdf -o book.pdf book.asciidoc
 
 cp -a book.pdf "$FILENAME"
 
