efc142360db4933685fc99bcd2724dc40e8c09f9
Fairiz Azizi <coderfi@gmail.com>
2018-07-31 00:51:04 -0700

    typo in build command

--- build_pdf.sh
+++ build_pdf.sh
@@ -15,7 +15,7 @@ FILENAME="Mastering Ethereum r${TAG}.pdf"
 git checkout book.asciidoc
 perl -pi -e "s/^= Mastering Ethereum$/= Mastering Ethereum r${TAG}/g" book.asciidoc
 perl -pi -e "s/:pdf-page-size: 8in x 9in/:pdf-page-size: 8in x 14in/g" book.asciidoc
-asciidoctor --r asciidoctor-pdf -b pdf -o book.pdf book.asciidoc
+asciidoctor -r asciidoctor-pdf -b pdf -o book.pdf book.asciidoc
 
 cp -a book.pdf "$FILENAME"
 
